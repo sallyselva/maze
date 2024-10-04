f1ff9fd7a27ac42a5ad5731d0358b04df14804b4
sallyselva <166712783+sallyselva@users.noreply.github.com>
2024-10-04 13:09:23 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLadder, function (sprite, location) {
+    info.setScore(carnival.getTimerValue())
+    info.score()
+    web.open("https://115.111.238.147:889/api/ECommReflection?playername=" + info.score() + "&score=" + info.score())
+    game.over(true)
+})
+let mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+tiles.setTilemap(tilemap`level_1`)
+tiles.placeOnRandomTile(mySprite, sprites.dungeon.stairWest)
+scene.cameraFollowSprite(mySprite)
+carnival.startTimer()
