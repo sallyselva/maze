6ce853f44f2459473a00be905c1ae34a76562b4d
sallyselva <166712783+sallyselva@users.noreply.github.com>
2024-10-04 14:38:23 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLadder, function (sprite, location) {
     info.setScore(carnival.getTimerValue())
     info.score()
-    web.open("https://115.111.238.147:889/api/ECommReflection?playername=" + info.score() + "&score=" + info.score())
+    web.open("https://115.111.238.147:889/api/ECommReflection?playername=" + info.score() + "&score=" + info.score() + "&gname=Maze")
     game.over(true)
 })
 let mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
