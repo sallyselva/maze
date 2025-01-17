cd3b9a5b79027f6a5ee062c78892542d5c2615cd
sallyselva <166712783+sallyselva@users.noreply.github.com>
2025-01-17 16:01:17 +0530

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLadder, function (sprite, location) {
     info.setScore(carnival.getTimerValue())
     info.score()
-    web.open("https://115.111.238.147:889/api/ECommReflection?playername=" + info.score() + "&score=" + info.score() + "&gname=Maze")
-    game.over(true)
+web.open("https://115.111.238.147:889/api/ECommReflection?playername=" + info.score() + "&score=" + info.score() + "&gname=Maze")
+game.over(true)
 })
-let mySprite = sprites.create(assets.image`myImage1`, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`myImage2`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 100)
 tiles.setTilemap(tilemap`level_1`)
 tiles.placeOnRandomTile(mySprite, sprites.dungeon.stairWest)
